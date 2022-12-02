6ad549bc11ed4ebc0250dc857d10376a95598b42
Mhasan1690 <39091988+Mhasan1690@users.noreply.github.com>
2022-12-02 15:33:13 +0600

    Add files via upload

---
+++ Question2.py
@@ -0,0 +1,7 @@
+
+
+list=[1, 5, 8, 12, "mehedi", 'Hasan', "3.1416"]
+
+Reversed_list = list[::-1]
+
+print( "Reverse a list without the reverse method", Reversed_list)
---
+++ Qusestion3.py
@@ -0,0 +1,8 @@
+
+#Reverse a list in Python (without the reverse method)
+
+list1= [4, 3, 6, 12, 15]
+
+list1.insert(2, "33")
+
+print(list1)  
